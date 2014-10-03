904efaca40fedcb03b01261cbd4e81347cbdb799
David Kim <hhjj0506@gmail.com>
2014-10-03 14:56:43 -0700

    First Commit

---
+++ public_html/js/snake.js
@@ -0,0 +1,43 @@
+var snake;
+var snakeLength;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+gameDraw();
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(0, 32, 58)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
+}
